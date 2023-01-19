e531a43ec04eda90c260e9507ac5eb71bf8de578
Tim Ganter <1911000+timganter@users.noreply.github.com>
2023-01-19 11:51:40 -0800

    Make cookie buttons.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let eatCookieResponse: any;
+
+	const eatCookie = async () => {
+		const response = await fetch('http://localhost:3333/eat-cookie');
+		eatCookieResponse = await response.json();
+	};
+
+	let bakeCookieResponse: any;
+
+	const bakeCookie = async () => {
+		const response = await fetch('http://localhost:3333/bake-cookie');
+		bakeCookieResponse = await response.json();
+	};
+
+	console.log('bakeCookieResponse', bakeCookieResponse);
+	console.log('eatCookieResponse', eatCookieResponse);
+</script>
+
+<button on:click={bakeCookie}>Bake Cookie</button>
+
+<button on:click={eatCookie}>Eat Cookie</button>
+
+<h2>Bake Cookie Response</h2>
+
+<code>{bakeCookieResponse?.value}</code>
+
+<h2>Eat Cookie Response</h2>
+
+<code>{eatCookieResponse?.value}</code>
